2a589dd3dc2cca1fdee49255cb11baf994632468
Katja Lutz <mail@katjalutz.ch>
2020-05-21 14:10:35 +0200

    feat: beautify client bundle and use otion

---
+++ lib/client/bundle.jsx
@@ -0,0 +1,41 @@
+// @deno-types=https://deno.land/std/react/react.d.ts
+import { h, render } from "preact";
+import { css, setup, hydrate } from "otion";
+
+setup({});
+hydrate();
+
+const NavClass = css({
+  display: "flex",
+  justifyContent: "center",
+});
+
+const ButtonClass = css({
+  padding: "1rem 3rem",
+  color: "#234",
+  textDecoration: "none",
+  fontSize: "1.7rem",
+  marginRight: "2rem",
+  ":last-child": {
+    marginRight: "0",
+  },
+  ":hover": {
+    backgroundColor: "#357",
+    color: "#fffe",
+  },
+});
+
+const Button = function ({ children, ...props }) {
+  return <a target="_blank" class={ButtonClass} {...props}>{children}</a>;
+};
+
+const Nav = function () {
+  return (<nav class={NavClass}>
+    <Button href="https://github.com/nexojs/nexo">🎒 Nexo</Button>&nbsp;
+    <Button href="https://github.com/oakserver/oak">🌳 Oak</Button>&nbsp;
+    <Button href="https://github.com/preactjs/preact">⚛️ Preact</Button>&nbsp;
+    <Button href="https://github.com/kripod/otion">🌊 Otion</Button>&nbsp;
+  </nav>);
+};
+
+render(<Nav />, document.querySelector("#nav"));
